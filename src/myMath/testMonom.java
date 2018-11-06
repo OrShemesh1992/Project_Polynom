4dc41043578f1bfb171d0cec22b02abfd3c9318d
or <chikos2011@gmail.com>
2018-11-06 18:22:52 +0200

    test6

--- src/myMath/testMonom.java
+++ src/myMath/testMonom.java
@@ -6,7 +6,7 @@ public class testMonom {
 		/**
 		 * testing monom constructor
 		 */
-		Monom a1 = new Monom(-2.2 , 1);
+		Monom a1 = new Monom(-2.8 , 1);
 		System.out.println("Monom constructor: " + a1); 
 		/**
 		 * testing copy constructor
